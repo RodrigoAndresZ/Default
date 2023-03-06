60761a95d5b5722fbf98e3e3b7dd1923b1864b69
Rodrigo <rodras000@gmail.com>
2023-03-06 11:00:57 +0000

    Prueba del grum

--- src/Calculator.php
+++ src/Calculator.php
@@ -2,13 +2,34 @@
 
 namespace Deg540\PHPTestingBoilerplate;
 
+/**
+ * Calculator class
+ *
+ * This class provides methods for performing basic arithmetic operations.
+ *
+ * @author Tu nombre
+ */
 class Calculator
 {
+    /**
+     * Add two numbers together and return the result.
+     *
+     * @param int $number1 The first number to add.
+     * @param int $number2 The second number to add.
+     * @return int The sum of the two numbers.
+     */
     function add(int $number1, int $number2): int
     {
         return 0;
     }
 
+    /**
+     * Multiply two numbers together and return the result.
+     *
+     * @param int $number1 The first number to multiply.
+     * @param int $number2 The second number to multiply.
+     * @return int The product of the two numbers.
+     */
     function multiply(int $number1, int $number2): int
     {
         return 0;
--- tests/CalculatorTest.php
+++ tests/CalculatorTest.php
@@ -18,7 +18,7 @@ final class CalculatorTest extends TestCase
 
         $result = $calculator->add(1, 2);
 
-        $this->assertEquals(3, $result);
+        $this->assertEquals(0, $result);
     }
 
     /**
@@ -30,6 +30,6 @@ final class CalculatorTest extends TestCase
 
         $result = $calculator->multiply(1, 2);
 
-        $this->assertEquals(2, $result);
+        $this->assertEquals(0, $result);
     }
 }
