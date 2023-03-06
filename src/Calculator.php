71cb0e18eede181d19f069a3dd7adcaeb55a8c94
Rodrigo <rodras000@gmail.com>
2023-03-06 12:51:44 +0000

    Prueba del gum 6

--- src/Calculator.php
+++ src/Calculator.php
@@ -32,6 +32,6 @@ class Calculator
      */
     public function multiply(int $number1, int $number2): int
     {
-        return ($number1 * $number2);
+        return ($number1*$number2);
     }
 }
